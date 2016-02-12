a37d8591a75dba5c8e7172556bf057d8aff54143
brett <thebrettd@gmail.com>
2016-02-11 20:33:04 -0800

    Fix bug with initial input error handling

--- src/main/java/com/tophatter/Main.java
+++ src/main/java/com/tophatter/Main.java
@@ -8,14 +8,18 @@ public class Main {
 
         System.out.println("Press 1 to play a game, or 2 to run the simulator:");
         Scanner scanner = new Scanner(System.in);
-        String choiceInput = scanner.nextLine();
 
         int choice = -1;
-        while(choice == -1){
+
+        while(!(choice == 1 || choice == 2)){
+            String choiceInput = scanner.nextLine();
             try{
                 choice = Integer.parseInt(choiceInput);
             }catch (NumberFormatException e){
                 System.out.println("Please enter 1 or 2");
+            }
+            if (!(choice == 1 || choice == 2)){
+                System.out.println(String.format("Invalid entry detected (%s), please enter 1 or 2", choice));
             }
         }
 
